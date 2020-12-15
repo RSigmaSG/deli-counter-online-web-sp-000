e3d6f896449a446170745da71acfec31e0c6cbd1
Sumer   Gaikwad <sg.comparch@gmail.com>
2020-12-15 03:28:16 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -2,6 +2,7 @@
 
 def line(arr)
   
-  arr.length == 0 ? return "The line is currently empty." : "The line is currently"
+  arr.length == 0 ? return "The line is currently empty." : return"The line is currently #{arr.each_with_index do |name|
+  "
   
 end
